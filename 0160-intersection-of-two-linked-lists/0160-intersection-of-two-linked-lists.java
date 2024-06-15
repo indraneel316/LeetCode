269805eff52ba41c8a612b24abb5791a1c99038c
indraneel316 <69248125+indraneel316@users.noreply.github.com>
2024-06-15 01:27:37 -0700

    Time: 1 ms (99.85%), Space: 48.3 MB (60.73%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -14,34 +14,32 @@ public class Solution {
         
         ListNode dummy1 = new ListNode();
         ListNode dummy2 = new ListNode();
-        ListNode dummy3 = new ListNode();
-        ListNode dummy4 = new ListNode();
         
         boolean isLen1 = false;
         int diff =0;
+        
         dummy1.next = headA;
         dummy2.next = headB;
-        
-        dummy3.next = headA;
-        dummy4.next = headB;
+
         
         ListNode curr1= dummy1;
         ListNode curr2 = dummy2;
         
-        ListNode curr3 = dummy1;
-        ListNode curr4 = dummy2;
-        
+
         int len1 =0;
         int len2 =0;
         
-        while(curr3.next!=null) {
+        while(curr1.next!=null) {
             len1++;
-            curr3 =curr3.next;
+            curr1 =curr1.next;
         }
-        while(curr4.next!=null) {
+        while(curr2.next!=null) {
             len2++;
-            curr4 =curr4.next;
+            curr2 =curr2.next;
         }
+        
+        curr1 = dummy1;
+        curr2 = dummy2;
         
         if(len1>len2) {
             isLen1 = true;
