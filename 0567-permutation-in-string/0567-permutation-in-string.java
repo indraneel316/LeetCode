9710c20276740b3f7b135dac4b2b74b394e479b7
indraneel316 <69248125+indraneel316@users.noreply.github.com>
2025-06-03 23:09:38 -0700

    Time: 23 ms (39.3%), Space: 43 MB (46.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,51 +1,43 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
-        
-        Map<Character, Integer> sMap1 = new HashMap<>();
-        for(char c : s1.toCharArray()) {
-            
-            if(sMap1.containsKey(c)) {
-                sMap1.put(c, sMap1.get(c)+1);
-            }
-            else {
-                 sMap1.put(c, 1);
-            }
-        }
-        
-        int i =0;
-        int j =0;
         int k = s1.length();
-        
-        int count = sMap1.size();
-        
-        while(j<s2.length()) {
-            if(sMap1.containsKey(s2.charAt(j))) {
-                sMap1.put(s2.charAt(j), sMap1.get(s2.charAt(j)) -1);
-                if(sMap1.get(s2.charAt(j)) == 0) {
-                    count--;
+        if (k > s2.length()) return false;
+
+        Map<Character, Integer> store = new HashMap<>();
+        for (char c : s1.toCharArray()) {
+            store.put(c, store.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0, formed = 0;
+
+        while (right < s2.length()) {
+            char c = s2.charAt(right);
+
+            if (store.containsKey(c)) {
+                store.put(c, store.get(c) - 1);
+                if (store.get(c) >= 0) {
+                    formed++;
                 }
             }
-             
-            if(j-i+1<k) {
-                j++;
-            }
-            
-            else if(j-i+1==k) {
-                
-                if(count ==0) {
-                    return true;
+
+            if (right - left + 1 > k) {
+                char leftChar = s2.charAt(left);
+                if (store.containsKey(leftChar)) {
+                    store.put(leftChar, store.get(leftChar) + 1);
+                    if (store.get(leftChar) > 0) {
+                        formed--;
+                    }
                 }
-                
-                if(sMap1.containsKey(s2.charAt(i))) {
-                   sMap1.put(s2.charAt(i), sMap1.get(s2.charAt(i))+1);
-                       if(sMap1.get(s2.charAt(i)) ==1) {
-                           count++;
-                       }
-                }
-                
-                i++;
-                j++;
+                left++;
             }
-               } return false;}
-               }
+
+            if (formed == k) {
+                return true;
+            }
+
+            right++;
+        }
+
+        return false;
+    }
+}
