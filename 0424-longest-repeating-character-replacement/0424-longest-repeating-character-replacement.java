ac86fb0180e77011e4f469c2a9e8b1920f067333
indraneel316 <69248125+indraneel316@users.noreply.github.com>
2025-06-03 22:06:17 -0700

    Time: 9 ms (74.77%), Space: 44.7 MB (71.9%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,43 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        
-        
-        int i=0;
-        int j=0;
-        int maxLen = Integer.MIN_VALUE;
-        int maxFreq = Integer.MIN_VALUE;
-        Map<Character, Integer> hmap = new HashMap<>();
-        // for(char c: s.toCharArray()) {
-        //     if(hMap.contains(c)) {
-        //         hMap.put(c, hMap.get(c)+1);
-        //     }
-        // }
-        
-        while(j<s.length()) {
-            if(hmap.containsKey(s.charAt(j))) {
-                hmap.put(s.charAt(j), hmap.get(s.charAt(j)) +1);
-            }
-            else {
-              hmap.put(s.charAt(j),1);
-            }
-            maxFreq = Math.max(maxFreq, hmap.get(s.charAt(j)));
+        int left =0;
+        int right =0;
+        int maxOccurence =0;
+        int ans =0;
+
+        int[] occurence = new int[26];
+
+        for(right =0; right<s.length();right++) {
+
+            maxOccurence = Math.max(maxOccurence , ++occurence[s.charAt(right)- 'A']);
+
             
-            if((j-i)+1- maxFreq>k) {
-            if(hmap.containsKey(s.charAt(i))) {
-                hmap.put(s.charAt(i), hmap.get(s.charAt(i)) -1);
+
+            if(right -left +1 - maxOccurence > k) {
+                --occurence[s.charAt(left)-'A'];
+                left++;
             }
-                maxFreq = 0;
-                i++;
-                j++;
-            }
-            
-            else if((j-i)+1 -maxFreq<=k) {
-                maxLen = Math.max(maxLen, j-i+1);
-                j++;
-            }
-            
-            
+
+            ans= Math.max(ans, right-left+1);
         }
-        return maxLen;
+
+        return ans;
+
+
+
     }
 }
